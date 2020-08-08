683df8d055a820b5cba6ee1eb7c0c270cb9ced4e
hakataminamiWS <hakataminami.web.service@gmail.com>
2020-08-08 20:55:43 +0900

    ダイクストラ法の実装、最短ルートの各頂点表示機能の実装、および任意のルートが到達可能かどうかの判定機能の実装

--- src/main/scala/ShortestPath.scala
+++ src/main/scala/ShortestPath.scala
@@ -5,78 +5,126 @@ case class Edge(from: Char, to: Char, distance: Int)
 
 object ShortestPath {
 
-  /**
-    * 頂点
-    */
-  val vertexes = 'A' to 'N'
+    /**
+      * 頂点
+      */
+    val vertexes = 'A' to 'N'
 
-  /**
-    * 辺
-    */
-  val edges = Seq(
-    Edge('A', 'B', 9),
-    Edge('A', 'C', 6),
-    Edge('A', 'D', 6),
-    Edge('B', 'A', 9),
-    Edge('B', 'E', 2),
-    Edge('C', 'A', 6),
-    Edge('C', 'E', 9),
-    Edge('C', 'G', 6),
-    Edge('D', 'A', 6),
-    Edge('D', 'F', 3),
-    Edge('E', 'B', 2),
-    Edge('E', 'C', 9),
-    Edge('E', 'I', 1),
-    Edge('F', 'D', 3),
-    Edge('F', 'H', 5),
-    Edge('F', 'J', 9),
-    Edge('G', 'C', 6),
-    Edge('G', 'I', 3),
-    Edge('G', 'J', 9),
-    Edge('H', 'F', 5),
-    Edge('H', 'K', 5),
-    Edge('I', 'E', 1),
-    Edge('I', 'G', 3),
-    Edge('J', 'F', 9),
-    Edge('J', 'G', 9),
-    Edge('J', 'K', 4),
-    Edge('J', 'L', 7),
-    Edge('J', 'M', 6),
-    Edge('K', 'H', 5),
-    Edge('K', 'J', 4),
-    Edge('K', 'M', 1),
-    Edge('L', 'J', 7),
-    Edge('L', 'N', 3),
-    Edge('M', 'J', 6),
-    Edge('M', 'K', 1),
-    Edge('M', 'N', 2),
-    Edge('N', 'L', 3),
-    Edge('N', 'M', 2)
-  )
+    /**
+      * 辺
+      */
+    val edges = Seq(
+        Edge('A', 'B', 9),
+        Edge('A', 'C', 6),
+        Edge('A', 'D', 6),
+        Edge('B', 'A', 9),
+        Edge('B', 'E', 2),
+        Edge('C', 'A', 6),
+        Edge('C', 'E', 9),
+        Edge('C', 'G', 6),
+        Edge('D', 'A', 6),
+        Edge('D', 'F', 3),
+        Edge('E', 'B', 2),
+        Edge('E', 'C', 9),
+        Edge('E', 'I', 1),
+        Edge('F', 'D', 3),
+        Edge('F', 'H', 5),
+        Edge('F', 'J', 9),
+        Edge('G', 'C', 6),
+        Edge('G', 'I', 3),
+        Edge('G', 'J', 9),
+        Edge('H', 'F', 5),
+        Edge('H', 'K', 5),
+        Edge('I', 'E', 1),
+        Edge('I', 'G', 3),
+        Edge('J', 'F', 9),
+        Edge('J', 'G', 9),
+        Edge('J', 'K', 4),
+        Edge('J', 'L', 7),
+        Edge('J', 'M', 6),
+        Edge('K', 'H', 5),
+        Edge('K', 'J', 4),
+        Edge('K', 'M', 1),
+        Edge('L', 'J', 7),
+        Edge('L', 'N', 3),
+        Edge('M', 'J', 6),
+        Edge('M', 'K', 1),
+        Edge('M', 'N', 2),
+        Edge('N', 'L', 3),
+        Edge('N', 'M', 2)
+    )
 
-  def solveByBellmanFord(start: Char, goal: Char): Unit = {
-    // 各頂点までの距離の初期化
-    var distances = vertexes.map(v => (v -> Int.MaxValue)).toMap
-    distances = distances + (start -> 0)
+    def solveByBellmanFord(start: Char, goal: Char): Map[Char, Int] = {
+        // 各頂点までの距離の初期化
+        var distances = vertexes.map(v => (v -> Int.MaxValue)).toMap
+        distances = distances + (start -> 0)
 
-    var isUpdated = true
-    while (isUpdated) {
-      isUpdated = false
-      edges.foreach { e =>
-        if(distances(e.from) != Int.MaxValue
-          && distances(e.to) > distances(e.from) + e.distance) {
-          distances = distances + (e.to -> (distances(e.from) + e.distance))
-          isUpdated = true
+        var isUpdated = true
+        while (isUpdated) {
+            isUpdated = false
+            edges.foreach { e =>
+                if (distances(e.from) != Int.MaxValue
+                    && distances(e.to) > distances(e.from) + e.distance) {
+                    distances = distances + (e.to -> (distances(e.from) + e.distance))
+                    isUpdated = true
+                }
+            }
         }
-      }
+        distances
     }
 
-    println(distances)
-    println(distances(goal))
-  }
+    def solveByDijkstra(start: Char, goal: Char): Map[Char, Int] = {
+        // 各頂点までの距離の初期化
+        var distances = vertexes.map(v => (v -> Int.MaxValue)).toMap
+        distances = distances + (start -> 0)
+        var usedEdges: Set[Edge] = Set()
+        var isUpdated = true
+        while (isUpdated) {
+            isUpdated = false
+            edges.foreach { e =>
+                if (!usedEdges.contains(e)
+                    && distances(e.from) != Int.MaxValue
+                    && distances(e.to) > distances(e.from) + e.distance) {
+                    distances = distances + (e.to -> (distances(e.from) + e.distance))
+                    usedEdges = usedEdges + e
+                    isUpdated = true
+                }
+            }
+        }
+        distances
+    }
 
-  def solveByDijkstra(start: Char, goal: Char): Unit = {
-    ???
-  }
+    sealed abstract class solveMethod
+    case object BellmanFord extends solveMethod
+    case object Dijkstra extends solveMethod
 
+    //    最短ルートが通る各頂点を返す
+    def vertexesOnShortestRoute(start: Char, goal: Char, method: solveMethod): Seq[Char] = {
+        val shortestDistanceMap: Map[Char, Int] = method match {
+            case BellmanFord => solveByBellmanFord(start, goal)
+            case Dijkstra => solveByDijkstra(start, goal)
+        }
+
+        var revVertexesOnShortestRoute: Seq[Char] = Seq(goal)
+        while (revVertexesOnShortestRoute.last != start) edges.foreach { e => {
+            if (e.to == revVertexesOnShortestRoute.last
+                && e.distance == (shortestDistanceMap(e.to) - shortestDistanceMap(e.from))) revVertexesOnShortestRoute = revVertexesOnShortestRoute :+ e.from
+        }
+        }
+        revVertexesOnShortestRoute.reverse
+    }
+
+    //    与えられた頂点(Seq)が、到達可能であるか、および合計距離を返す
+    def showDistance(routeSeq: Seq[Char]): (Boolean, Int) = {
+        var isReachable: Boolean = true
+        var totalDistance: Int = 0
+        val routePair: Seq[(Char, Char)] = routeSeq zip routeSeq.tail
+        routePair.foreach { case (from, to) =>
+            edges.find(e => (e.from == from) && (e.to == to)) match {
+                case Some(edge) => totalDistance = totalDistance + edge.distance
+                case None => isReachable = false; totalDistance = 0
+            }
+        }
+        (isReachable, totalDistance)
+    }
 }
